029794d40a207952a4d01b6d37a2f1a047eb48da
Hallel9 <47569515+Hallel9@users.noreply.github.com>
2021-02-23 17:48:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -32,56 +32,35 @@ client.on('ready', () => {
 client.on('guildMemberAdd', async member => {
     const channel = member.guild.channels.cache.get('802323324772286506');
   if (!channel) return;
-const { ClientUser } = require('discord.js');
-const Commando = require('discord.js-commando');
-const path = require('path');
-const config = require('./config.json')
-const client = new Commando.Client({
-    owner: config.ownerId,
-    commandPrefix: config.prefix,
-    disableEveryone: true
+
+channel.send(`${member} has sucsessfully logged in!`)
 });
-client.login(config.token);
-client.registry
-    // Registers your custom command groups
-    .registerGroups([
-        ['moderation', 'Moderation commands'],
-        ['misc', 'Misc Commands'],
-        ['music', 'Music commands'],
-        ['owneronly', 'Owner Only Commands'],
-        ['roles', 'Roles Commands'],
-        ['embed', 'Embed example commands']
-    ])
 
-    // Registers all built-in groups, commands, and argument types
-    .registerDefaults()
+client.on('guildMemberRemove', async member => {
+    const channel = member.guild.channels.cache.get('802323324772286506');
+  if (!channel) return;
 
-    // Registers all of your commands in the ./commands/ directory
-    .registerCommandsIn(path.join(__dirname, 'commands'));
+channel.send(`${member.user.tag} has logged out!`)
+});
 
-client.on('ready', () => {
-    console.log(`${client.user.username} has successfully logged in!`);
+channel_id = '808335210788683777' // ID here
+
+client.on('guildCreate', (guild) => {
+  const notice = {
+    title: `New server`,
+    description: `I've been added to ${guild.name}`,
+    thumbnail: { url: guild.iconURL() },
+    timestamp: Date.now()
+  }
+  client.channels.cache.get(channel_id).send({ embed: notice })
 })
 
-client.on('guildMemberAdd', async member => {
-    const channel = member.guild.channels.cache.get('802323324772286506');
-  if (!channel) return;
-
-channel.send(`${member} has sucsessfully logged in!`)
-});
-
-client.on('guildMemberRemove', async member => {
-    const channel = member.guild.channels.cache.get('802323324772286506');
-  if (!channel) return;
-
-channel.send(`${member.user.tag} has logged out!`)
-});
-channel.send(`${member} has sucsessfully logged in!`)
-});
-
-client.on('guildMemberRemove', async member => {
-    const channel = member.guild.channels.cache.get('802323324772286506');
-  if (!channel) return;
-
-channel.send(`${member.user.tag} has logged out!`)
-});
+client.on('guildDelete', (guild) => {
+  const notice = {
+    title: `New server`,
+    description: `I've been removed to ${guild.name}`,
+    thumbnail: { url: guild.iconURL() },
+    timestamp: Date.now()
+  }
+  client.channels.cache.get(channel_id).send({ embed: notice })
+})
