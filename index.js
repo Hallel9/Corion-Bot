1e523a73f99789f56ea165487eb730d91a8dbb0d
Hallel9 <47569515+Hallel9@users.noreply.github.com>
2021-02-23 17:41:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const { ClientUser } = require('discord.js');
 const Commando = require('discord.js-commando');
 const path = require('path');
 const config = require('./config.json')
 const client = new Commando.Client({
     owner: config.ownerId,
-    commandPrefix: config.prefix
+    commandPrefix: config.prefix,
+    disableEveryone: true
 });
 client.login(config.token);
 client.registry
@@ -12,7 +14,9 @@ client.registry
         ['moderation', 'Moderation commands'],
         ['misc', 'Misc Commands'],
         ['music', 'Music commands'],
-        ['owneronly', 'Owner Only Commands']
+        ['owneronly', 'Owner Only Commands'],
+        ['roles', 'Roles Commands'],
+        ['embed', 'Embed example commands']
     ])
 
     // Registers all built-in groups, commands, and argument types
@@ -22,5 +26,62 @@ client.registry
     .registerCommandsIn(path.join(__dirname, 'commands'));
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.username}`);
+    console.log(`${client.user.username} has successfully logged in!`);
+})
+
+client.on('guildMemberAdd', async member => {
+    const channel = member.guild.channels.cache.get('802323324772286506');
+  if (!channel) return;
+const { ClientUser } = require('discord.js');
+const Commando = require('discord.js-commando');
+const path = require('path');
+const config = require('./config.json')
+const client = new Commando.Client({
+    owner: config.ownerId,
+    commandPrefix: config.prefix,
+    disableEveryone: true
+});
+client.login(config.token);
+client.registry
+    // Registers your custom command groups
+    .registerGroups([
+        ['moderation', 'Moderation commands'],
+        ['misc', 'Misc Commands'],
+        ['music', 'Music commands'],
+        ['owneronly', 'Owner Only Commands'],
+        ['roles', 'Roles Commands'],
+        ['embed', 'Embed example commands']
+    ])
+
+    // Registers all built-in groups, commands, and argument types
+    .registerDefaults()
+
+    // Registers all of your commands in the ./commands/ directory
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.on('ready', () => {
+    console.log(`${client.user.username} has successfully logged in!`);
+})
+
+client.on('guildMemberAdd', async member => {
+    const channel = member.guild.channels.cache.get('802323324772286506');
+  if (!channel) return;
+
+channel.send(`${member} has sucsessfully logged in!`)
+});
+
+client.on('guildMemberRemove', async member => {
+    const channel = member.guild.channels.cache.get('802323324772286506');
+  if (!channel) return;
+
+channel.send(`${member.user.tag} has logged out!`)
+});
+channel.send(`${member} has sucsessfully logged in!`)
+});
+
+client.on('guildMemberRemove', async member => {
+    const channel = member.guild.channels.cache.get('802323324772286506');
+  if (!channel) return;
+
+channel.send(`${member.user.tag} has logged out!`)
 });
