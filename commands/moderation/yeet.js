ae5d99e3f1b383f9b51f470cd6036b131ba8c4f8
Hallel9 <47569515+Hallel9@users.noreply.github.com>
2021-03-03 23:56:14 -0500

    Update and rename ban.js to yeet.js

--- commands/moderation/yeet.js
+++ commands/moderation/yeet.js
@@ -3,9 +3,9 @@ const Commando = require('discord.js-commando')
 module.exports = class BanCommand extends Commando.Command {
   constructor(client) {
     super(client, {
-      name: 'ban',
+      name: 'yeet',
       group: 'moderation',
-      memberName: 'ban',
+      memberName: 'yeet',
       description: 'Bans a member from the discord server',
       clientPermissions: ['BAN_MEMBERS'],
       userPermissions: ['BAN_MEMBERS'],
@@ -15,18 +15,23 @@ module.exports = class BanCommand extends Commando.Command {
   async run(message) {
     const target = message.mentions.users.first()
     if (!target) {
-      message.reply('Please specify someone to ban!')
+      message.reply('Please specify someone to yeet!')
       return
     }
 
     const { guild } = message
+    const author = message.author
 
     const member = guild.members.cache.get(target.id)
     if (member.bannable) {
       member.ban()
-      message.reply('That user has been banned')
+      const embed = new Discord.MessageEmbed()
+        .setTitle('Yeeted!')
+        .setDescription(`${member} has been yeeted by ${author}!`)
+        .setColor('RANDOM')
+      message.channel.send(embed)
     } else {
-      message.reply('I cannot ban that user')
+      message.reply('I cannot yeet that user')
     }
   }
 }
