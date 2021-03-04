b24eb0944de698299671da9726a0fa52999bc85c
Hallel9 <47569515+Hallel9@users.noreply.github.com>
2021-03-03 23:55:52 -0500

    Update and rename kick.js to skirt.js

--- commands/moderation/skirt.js
+++ commands/moderation/skirt.js
@@ -1,32 +1,41 @@
 const Commando = require('discord.js-commando')
+const Discord = require('discord.js')
 
 module.exports = class KickCommand extends Commando.Command {
   constructor(client) {
     super(client, {
-      name: 'kick',
+      name: 'skirt',
       group: 'moderation',
-      memberName: 'kick',
+      memberName: 'skirt',
       description: 'Kicks a member from the discord server',
       clientPermissions: ['KICK_MEMBERS'],
       userPermissions: ['KICK_MEMBERS'],
+      usage: '>skirt <@user>',
+      examples: ['${prefix}skirt @josh'],
     })
   }
 
   async run(message) {
     const target = message.mentions.users.first()
     if (!target) {
-      message.reply('Please specify someone to kick')
+      message.reply('Please specify someone to skirt')
       return
     }
 
     const { guild } = message
 
     const member = guild.members.cache.get(target.id)
+    
+    const author = message.author
     if (member.kickable) {
       member.kick()
-      message.reply('That user has been kicked')
+      const embed = new Discord.MessageEmbed()
+        .setTitle('Skirt!')
+        .setDescription(`${member} has been yeeted by ${author}!`)
+        .setColor('RANDOM')
+      message.channel.send(embed)
     } else {
-      message.reply('I cannot kick that user')
+      message.reply('I cannot skirt that user')
     }
   }
 }
